ecab8253b52e9552a382b1a9028195a0f4ce1070
William Phan <phnam230197@gmail.com>
2025-08-11 01:17:32 +0700

    repo
    add missing initial for module tbirpg2d

--- tbirpg2d/src/test/kotlin/vn/chuot96/tbirpg2d/Tbirpg2dTests.kt
+++ tbirpg2d/src/test/kotlin/vn/chuot96/tbirpg2d/Tbirpg2dTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class Tbirpg2dApplicationTests {
+class Tbirpg2dTests {
 
 	@Test
 	fun contextLoads() {
