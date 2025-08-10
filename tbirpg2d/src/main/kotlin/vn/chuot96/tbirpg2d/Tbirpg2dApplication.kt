9a3cc0a555ea7541aff40a4905a98206ff34e3e4
William Phan <phnam230197@gmail.com>
2025-08-11 00:58:24 +0700

    repo
    initial module tbirpg2d

---
+++ tbirpg2d/src/main/kotlin/vn/chuot96/tbirpg2d/Tbirpg2dApplication.kt
@@ -0,0 +1,11 @@
+package vn.chuot96.tbirpg2d
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Tbirpg2dApplication
+
+fun main(args: Array<String>) {
+	runApplication<Tbirpg2dApplication>(*args)
+}
---
+++ tbirpg2d/src/test/kotlin/vn/chuot96/tbirpg2d/Tbirpg2dApplicationTests.kt
@@ -0,0 +1,13 @@
+package vn.chuot96.tbirpg2d
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Tbirpg2dApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
