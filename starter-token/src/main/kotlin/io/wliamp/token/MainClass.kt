fced4b8c0e92d495712666ce97cd2fe84c1a2794
William Phan <phnam230197@gmail.com>
2025-08-14 09:21:16 +0700

    repo
    initial module starter-token

---
+++ starter-token/src/main/kotlin/io/wliamp/token/MainClass.kt
@@ -0,0 +1,11 @@
+package io.wliamp.token
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarterTokenApplication
+
+fun main(args: Array<String>) {
+	runApplication<StarterTokenApplication>(*args)
+}
---
+++ starter-token/src/test/kotlin/io/wliamp/token/TestsClass.kt
@@ -0,0 +1,13 @@
+package io.wliamp.token
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestsClass {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
