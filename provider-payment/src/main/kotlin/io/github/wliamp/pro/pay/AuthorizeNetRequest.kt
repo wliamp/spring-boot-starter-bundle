1c29f14c93fcd607f5018968eb3d0805e6a3bbf7
William Phan <phnam230197@gmail.com>
2025-08-30 05:27:09 +0700

    feat(pro-pay): Builder support for data classes

--- provider-payment/src/main/kotlin/io/github/wliamp/pro/pay/AuthorizeNetRequest.kt
+++ provider-payment/src/main/kotlin/io/github/wliamp/pro/pay/AuthorizeNetRequest.kt
@@ -4,7 +4,32 @@ import io.github.wliamp.pro.pay.gtw.ORequest
 
 data class AuthorizeNetRequest(
     val amount: String,
-    val refTransId: String,
+    val description: String?,
     val orderId: String,
-    val description: String?
-): ORequest()
+    val refTransId: String,
+) : ORequest() {
+    companion object {
+        @JvmStatic
+        fun builder() = Builder()
+    }
+
+    class Builder {
+        private var amount: String = ""
+        private var description: String? = null
+        private var orderId: String = ""
+        private var refTransId: String = ""
+
+        fun amount(amount: String) = apply { this.amount = amount }
+        fun description(description: String?) = apply { this.description = description }
+        fun orderId(orderId: String) = apply { this.orderId = orderId }
+        fun refTransId(refTransId: String) = apply { this.refTransId = refTransId }
+
+        fun build() = AuthorizeNetRequest(
+            amount,
+            description,
+            orderId,
+            refTransId
+        )
+    }
+}
+
