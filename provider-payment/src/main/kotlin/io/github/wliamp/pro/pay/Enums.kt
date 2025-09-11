67b1dbbed8ce9068c50b83db76393037237715b7
K. Windsor <namrolex1998@gmail.com>
2025-09-11 21:43:52 +0700

    feat(pro-pay): clarity method call with enums name by PaymentProvider.of

---
+++ provider-payment/src/main/kotlin/io/github/wliamp/pro/pay/Enums.kt
@@ -0,0 +1,7 @@
+package io.github.wliamp.pro.pay
+
+enum class Payment {
+    AUTHORIZE_NET,
+    VN_PAY,
+    ZALO_PAY
+}
--- provider-payment/src/main/kotlin/io/github/wliamp/pro/pay/PaymentProvider.kt
+++ provider-payment/src/main/kotlin/io/github/wliamp/pro/pay/PaymentProvider.kt
@@ -1,10 +1,14 @@
 package io.github.wliamp.pro.pay
 
-import org.springframework.stereotype.Component
-
-@Component
 class PaymentProvider(
     val authorizeNet: IPay<AuthorizeNetClientData, AuthorizeNetSystemData>?,
     val vnPay: IPay<VnPayClientData, VnPaySystemData>?,
     val zaloPay: IPay<ZaloPayClientData, ZaloPaySystemData>?
-)
+) {
+    fun of(payment: Payment): IPay<*, *>? =
+        when (payment) {
+            Payment.AUTHORIZE_NET -> authorizeNet
+            Payment.VN_PAY -> vnPay
+            Payment.ZALO_PAY -> zaloPay
+        }
+}
